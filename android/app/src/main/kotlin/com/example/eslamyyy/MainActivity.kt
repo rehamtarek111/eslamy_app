c2165fb64c05cdbcb5be7fa47855384f8e18dcf1
reham <reham.tarek.inbox@gmail.com>
2024-02-10 01:52:56 -0800

    initial project add empty home screen

---
+++ android/app/src/main/kotlin/com/example/eslamyyy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eslamyyy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
---
+++ lib/home/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  static const String routeName = "home-screen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
